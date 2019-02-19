a1c98944adf1d29136e61a749597d6d9bf574229
Unknown <bhardwaj.vishesh@outlook.com>
2019-02-19 13:47:33 +0530

    Begin implementing CORS Configuration

---
+++ src/main/java/com/rest/configuration/CORSConfiguration.java
@@ -0,0 +1,9 @@
+package com.rest.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class CORSConfiguration extends RepositoryRestConfigurerAdapter {
+
+}
