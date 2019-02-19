ae5f49bfc10bc255ddc27bb20f8161accbc26811
Unknown <bhardwaj.vishesh@outlook.com>
2019-02-19 11:50:13 +0530

    Changes To Spring Security To enable Database - based Authentication

--- src/main/java/com/rest/configuration/SecurityConfiguration.java
+++ src/main/java/com/rest/configuration/SecurityConfiguration.java
@@ -1,43 +1,74 @@
 package com.rest.configuration;
 
+import com.rest.repository.UserRepository;
+import com.rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+ @Autowired
+ private UserService userService;
+
  @Override
  protected void configure(HttpSecurity http) throws Exception {
-  http.csrf().disable();
+  http.cors().and().csrf().disable();
+
   http.authorizeRequests()
+      .antMatchers("/login*").permitAll()
       .anyRequest().authenticated()
       .and()
-      .formLogin()
-      .and()
-      .httpBasic();
+      .formLogin().permitAll();
+      //.loginProcessingUrl("/login")
+      //.defaultSuccessUrl("/browser",true).successForwardUrl("/browser").failureForwardUrl("/login")
+      //.permitAll();
+
+     /*http.formLogin()
+             .loginProcessingUrl("/login")
+             .defaultSuccessUrl("/browser/index.html",true)
+             .failureUrl("/login?error=true")
+     .and().authenticationProvider(authenticationProvider()).userDetailsService(userService).httpBasic();*/
  }
 
- @Autowired
+ @Override
+ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+  auth.inMemoryAuthentication()
+             .withUser("admin").password(passwordEncoder().encode("rdt_user"))
+             .authorities("Admin");
+ }
+
+/* @Autowired
  protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
   auth.inMemoryAuthentication()
-          .withUser("user").password(passwordEncoder().encode("password"))
+          .withUser("user").password(passwordEncoder().encode("user"))
           .authorities("ROLE_USER");
- }
+
+ }*/
 
  @Bean
  public PasswordEncoder passwordEncoder() {
-  return new BCryptPasswordEncoder();
+  return new PasswordEncoder() {
+      @Override
+      public String encode(CharSequence rawPassword) {
+          return rawPassword.toString();
+      }
+
+      @Override
+      public boolean matches(CharSequence rawPassword, String encodedPassword) {
+          return true;
+      }
+  };
  }
 
 }
