fa308c4bcd724adc4c025e26b5a6d5d03f46dfcc
Unknown <bhardwaj.vishesh@outlook.com>
2019-04-26 09:48:50 +0530

    --UPDATE-- Spring To Latest Release

--- src/main/java/com/rest/configuration/CORSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.rest.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-
-@Configuration
-public class CORSConfiguration extends RepositoryRestConfigurerAdapter {
-
-}
